ca9d0bb55113d4da63d221fe645a663de77b485a
muzudho <muzudho1@gmail.com>
2026-06-13 23:06:37 +0900

    ［符牒の破片］と［宝石］の位置が偏っていたので、混ぜてもらったぜ（＾▽＾）

--- SkylarkBimbleStreet/Stage.cs
+++ SkylarkBimbleStreet/Stage.cs
@@ -34,9 +34,45 @@ internal sealed class Stage
         HospitalBounds = hospitalBounds;
         BackgroundColor = backgroundColor;
         Walls = walls;
-        var ticketPieceCount = Math.Min(3, collectibles.Length);
-        TicketPieces = collectibles[..ticketPieceCount];
-        Gems = collectibles[ticketPieceCount..];
+        var ticketPieceIndexes = ChooseTicketPieceIndexes(collectibles.Length);
+        TicketPieces = new Rectangle[ticketPieceIndexes.Length];
+        Gems = new Rectangle[collectibles.Length - ticketPieceIndexes.Length];
+
+        var ticketPieceIndex = 0;
+        var gemIndex = 0;
+        for (var i = 0; i < collectibles.Length; i++)
+        {
+            if (ticketPieceIndex < ticketPieceIndexes.Length && i == ticketPieceIndexes[ticketPieceIndex])
+            {
+                TicketPieces[ticketPieceIndex] = collectibles[i];
+                ticketPieceIndex++;
+                continue;
+            }
+
+            Gems[gemIndex] = collectibles[i];
+            gemIndex++;
+        }
         Hazards = hazards;
+    }
+
+    private static int[] ChooseTicketPieceIndexes(int collectibleCount)
+    {
+        var ticketPieceCount = Math.Min(3, collectibleCount);
+        if (ticketPieceCount == 0)
+        {
+            return [];
+        }
+
+        if (ticketPieceCount == 1)
+        {
+            return [0];
+        }
+
+        if (ticketPieceCount == 2)
+        {
+            return [0, collectibleCount - 1];
+        }
+
+        return [0, collectibleCount / 2, collectibleCount - 1];
     }
 }
